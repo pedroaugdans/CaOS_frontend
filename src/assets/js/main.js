f2d5787ce5f64f1fad90950683dbe05ac0cb57c8
pedroaugdans <pedro.aug.dans@gmail.com>
2024-05-26 18:18:25 +0200

    fixed top left icon

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -20,7 +20,7 @@
           "assets/images/logo/logo.svg"
       } else {
         document.querySelector(".header-logo").src =
-          "assets/images/logo/logo-white.svg"
+          "assets/images/logo/logo.svg"
       }
     }
 
@@ -29,8 +29,8 @@
         // === logo change
         if (ud_header.classList.contains("sticky")) {
           document.querySelector(".header-logo").src =
-            "assets/images/logo/logo-white.svg"
-        } 
+            "assets/images/logo/logo.svg"
+        }
       }
     }
 
